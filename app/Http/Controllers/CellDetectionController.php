bff57963504f0d60f964730f8a9c5fe0710c163a
Hakan Kurtulus <hakan.kurtulus@sap.com>
2018-11-20 10:19:11 +0100

    deploy

--- app/Http/Controllers/CellDetectionController.php
+++ app/Http/Controllers/CellDetectionController.php
@@ -6,30 +6,84 @@ use Illuminate\Http\Request;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use PhotoUploadHelper;
+use Illuminate\Support\Facades\Storage;
 
 class CellDetectionController extends Controller
 {
+    public $types = array(
+        1 => 'red',
+        2 => 'platelet',
+        3 => 'basophils',
+        4 => 'eosinophils',
+        5 => 'lymphocytes',
+        6 => 'monocytes',
+        7 => 'neutrophils',
+    );
+
     public function index() 
     {
-        return view('index');
+        $file_name = '1542573406_RouQx9R3Cy.jpg';
+        $file_name_arr = explode(".", $file_name);
+
+        $directory = 'public/images/'.$file_name_arr[0];
+        $files = Storage::disk('local')->files($directory);
+
+        $crops = collect();
+        foreach ($files as $key => $file_path) {
+            $file_path_arr = explode('/', $file_path);
+            $crop_name = $file_path_arr[count($file_path_arr)-1];
+            $crop_name_arr = explode('.', $crop_name);
+            $crop_details_arr = explode('_', $crop_name_arr[0]);
+            $crop_score = $crop_details_arr[0];
+            $crop_type = $crop_details_arr[1];
+            $crop_type = $this->types[$crop_type];
+            $crops[] = array(
+                'score' => $crop_score, 
+                'type' => $crop_type, 
+                'source' => $file_name_arr[0].'/'.$crop_name
+            );
+        }
+        return view('index')
+            ->with('crops', $crops);
     }
 
     public function upload(Request $request) 
     {
         $file_name = PhotoUploadHelper::upload($request->image);
+        $this->run($file_name, $request->type);
+        $file_name_arr = explode(".", $file_name);
 
-        $this->run($file_name);
+        $directory = 'public/images/'.$file_name_arr[0];
+        $files = Storage::disk('local')->files($directory);
 
-        return $file_name;
+        $crops = array();
+        foreach ($files as $key => $file_path) {
+            $file_path_arr = explode('/', $file_path);
+            $crop_name = $file_path_arr[count($file_path_arr)-1];
+            $crop_name_arr = explode('.', $crop_name);
+            $crop_details_arr = explode('_', $crop_name_arr[0]);
+            $crop_score = $crop_details_arr[0];
+            $crop_type = $crop_details_arr[1];
+            $crop_type = $this->types[$crop_type];
+            $crops[] = array(
+                'score' => $crop_score, 
+                'type' => $crop_type, 
+                'source' => $file_name_arr[0].'/'.$crop_name
+            );
+        }
+
+        return response()->json(['file' => $file_name, 'crops' => $crops]);
+
     }
 
-    public function run($file_name) 
+    public function run($file_name, $type) 
     {
-        if (config('app.env' == 'local')) {
-            $process = new Process("python /home/vagrant/code/CellDetection/python/models/research/object_detection/test.py ".$file_name);
-        } else {
-            $process = new Process("python /var/www/bloodcell.xyz/python/models/research/object_detection/test.py ".$file_name);
-        }
+        // if (config('app.env' == 'local')) {
+            // $process = new Process("python /home/vagrant/code/CellDetection/python/models/research/object_detection/test.py ".$file_name." ".$type);
+        // } else {
+            $process = new Process("python /var/www/bloodcell.xyz/python/models/research/object_detection/test.py ".$file_name." ".$type);
+        // }
+        $process->setTimeout(3600);
         $process->run();
 
         if (!$process->isSuccessful()) {
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,44 +6,98 @@
 
 <section class="bd-index-fullscreen hero is-fullheight is-light">
     <div class="hero-head">
-      <div class="container">
+      <div class="container" style="    margin: 16px auto 0px auto;">
         <div class="tabs is-centered">
           <ul>
-            <li><a>Blood Cell Type Detection</a></li>
+            <li>
+                <h5 class="title is-5">Blood Cell Type Detection</h5>
+            </li>
           </ul>
         </div>
       </div>
     </div>
     <div class="hero-body">
-      <div class="container">
-          <div class="columns">
-              <div class="column is-12 has-text-centered">
-                <b-upload v-if="!showImage" v-model="file"
-                    @input="onFileChange"
-                    drag-drop>
-                    <section class="section">
-                        <div class="content has-text-centered">
-                            <p>
-                                <b-icon
-                                    icon="upload"
-                                    size="is-large">
-                                </b-icon>
-                            </p>
-                            <p>Drop your files here or click to upload</p>
+      <div class="container" v-cloak>
+            <div class="columns">
+                <div class="column is-12 has-text-centered">
+                    <div v-if="!showImage" >
+                        <b-upload v-model="file"
+                            @input="onFileChange"
+                            drag-drop>
+                            <section class="section">
+                                <div class="content has-text-centered">
+                                    <p>
+                                        <b-icon
+                                            icon="upload"
+                                            size="is-large">
+                                        </b-icon>
+                                    </p>
+                                    <p>Drop your files here or click to upload</p>
+                                </div>
+                            </section>
+                        </b-upload>
+                        <div class="block" style="margin-top:20px;">
+                            <b-radio v-model="type"
+                                native-value="fast">
+                                SSD Mobilenet
+                            </b-radio>
+                            <b-radio v-model="type"
+                                native-value="slow">
+                                RCNN Resnet
+                            </b-radio>
                         </div>
-                    </section>
-                </b-upload>
-                <div v-if="showImage" class="avatar-wrapper">
-                    <img class="profile-pic has-text-centered" :src="image" alt="" />
-                    <div class="upload-button"></div>
+                    </div>
+                    
+                    <div class="columns">
+                        <transition name="slide-fade">
+                            <div v-if="showImage" class="column is-7">
+                                <div class="card">
+                                    <div class="card-image">
+                                        <figure class="image is-4by4">
+                                        <img :src="image" alt="Placeholder image">
+                                        </figure>
+                                    </div>
+                                    <div class="card-content">
+                                        <div class="content">
+                                            <a class="button is-primary" :href="image" target="blank">Download</a>
+                                            <button class="button is-grey" @click="showImage = false">Analyze Again</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </transition>
+                        <transition name="slide-fade-right">
+                            <div v-if="showImage" class="column is-5">
+                                <div class="columns is-multiline">
+                                    <div class="column is-6" v-for="(crop, index) in crops" :key="index">
+                                        <div class="card">
+                                            <div class="card-content">
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <figure class="image is-48x48" style="    overflow: hidden;">
+                                                            <img :src="'storage/images/'+crop.source" alt="Placeholder image">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="media-content">
+                                                        <p class="title is-4">@{{ crop.type | capitalize}}</p>
+                                                        <p class="subtitle is-6"> 
+                                                            <b-tag :type="crop.score | type">
+                                                                <span class="has-text-weight-bold">@{{ crop.score }}%</span> Precision
+                                                            </b-tag>
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>                        
+                        </transition>
+                        
+                    </div>
+                    <b-loading :is-full-page="true" :active.sync="isLoading" :can-cancel="false"></b-loading>
                 </div>
-                <div v-if="showImage">
-                    <a class="button is-primary" :href="image" target="blank">Download</a>
-                    <button class="button is-grey" @click="showImage = false">Again</button>
-                </div>
-                <b-loading :is-full-page="true" :active.sync="isLoading" :can-cancel="false"></b-loading>
-              </div>
-          </div>
+            </div>
       </div>
     </div>
   
@@ -64,9 +118,28 @@
     var Main = {
             data:{
                 file: null,
-                image: '',
+                image: 'storage/images/1542573406_RouQx9R3Cy.jpg',
+                crops: {!! json_encode($crops) !!},
+                type: 'fast',
                 isLoading: false,
-                showImage: false
+                showImage: true
+            },
+            filters: {
+                capitalize: function (string) {
+                    return string.charAt(0).toUpperCase() + string.slice(1);
+                },
+                type: function (value) {
+                    if (value >= 65) {
+                        return 'is-success';
+                    } else if(45 >= value <= 64) {
+                        return 'is-warning';
+                    } else {
+                        return ''
+                    }
+                }
+            },
+            mounted() {
+                this.crops = this.crops.reverse();
             },
             methods: {
                 onFileChange(e) {
@@ -88,17 +161,17 @@
                     that.isLoading = true;
                     axios.post("/upload", {
                         image: image,
+                        type: that.type
                     }).then(function (response) {
-                        that.image = 'storage/images/'+response.data;
+                        that.image = 'storage/images/'+response.data.file;
+                        that.crops = response.data.crops.reverse();
                         that.isLoading = false;
-                        that.showImage = true;
+                        that.showImage = true;                        
                         console.log(response.data);
-                        console.log(that.image);
-                        
                     })
                     .catch(function (error) {
                         that.isLoading = false;
-                        console.log(error);
+                        console.log(error.response);
                     });
                 },
             }
